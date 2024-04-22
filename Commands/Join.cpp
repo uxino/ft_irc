09b39b9607ab6d65b9eae903610293c12aaf7c25
Muhammed Esad Seker <museker@k1m04s06.42istanbul.com.tr>
2024-04-22 20:09:05 +0300

    mode is completed

--- Commands/Join.cpp
+++ Commands/Join.cpp
@@ -43,11 +43,18 @@ void    Server::Join(int index, int id)
         if (channels[i].getChannelName() == commands[index + 1])
         {
             for (size_t j = 0; j < channels[i].getClients().size(); j++)
-                channels[i].getClients()[j].print(":" + clients[id].getUserName() + "!" + clients[id].getUserName() + "@localhost" + " JOIN " + commands[index + 1] + "\r\n");
+            {
+                channels[i].getClients()[j].print(":" + clients[id].getUserName() + "!" + clients[id].getUserName() + "@"+ clients[id].getIp() + " JOIN " + commands[index + 1] + "\r\n");
+                channels[i].getClients()[j].print("MODE " + channels[i].getChannelName() + " +o " + channels[i].getClients()[0].getUserName() + "\r\n");
+
+            }
             for (size_t k = 0; k < channels[i].getClients().size(); k++)
             {
                 if (channels[i].getClients()[k].getNickName() != clients[id].getNickName())
+                {
                     clients[id].print(":" + channels[i].getClients()[k].getUserName() + "!" + channels[i].getClients()[k].getUserName() + "@localhost" + " JOIN " + commands[index + 1] + "\r\n");
+                    clients[id].print("MODE " + channels[i].getChannelName() + " +o " + channels[i].getClients()[0].getUserName() + "\r\n");
+                }
             }
         }
     }
